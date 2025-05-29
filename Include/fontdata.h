012b61d280863848e161cc2d9c50d115e4f10d78
Jason Benson <cto2_benson@hotmail.com>
2025-05-29 15:43:26 -0700

    Corrected some coding and prepped the save font to file functions it is not implemented yet.

--- Include/fontdata.h
+++ Include/fontdata.h
@@ -8,9 +8,11 @@
 struct FontData {
     static const int NUM_CHARACTERS = 256;
     static const int PATTERN_SIZE_BYTES = 8; // 8 bytes for 8x8 bits
+    static const int PATTERN_ROWS = 8;             // <--- ADD THIS LINE
+    static const int PATTERN_BITS_PER_ROW = 8;
 
-    std::array<std::bitset<8>, NUM_CHARACTERS> characterPatterns;
-
+   // std::array<std::bitset<8>, NUM_CHARACTERS> characterPatterns;
+    std::array<std::array<std::bitset<PATTERN_BITS_PER_ROW>, PATTERN_ROWS>, NUM_CHARACTERS> characterPatterns;
     FontData(); // Constructor to initialize the array
 };
 
